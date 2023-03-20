d1e35f00a91096e2d39a4f35981dc0428b25b462
Rew <irinavakulenko2000@gmail.com>
2023-03-20 14:06:58 +0200

    modal

--- js/modal_setIntrval.js
+++ js/modal_setIntrval.js
@@ -1,24 +1,60 @@
 import BSN  from "bootstrap.native";
 
-const modal = new BSN.modal('#exampleModal');
+const modal = new BSN.Modal('#exampleModal');
+
+const PROMPT_DELAY = 3000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
+
+openModal();
 
 console.log(modal);
 
-const startBtn = document.querySelector(".js-start");
-const stopBtn = document.querySelector(".js-stop");
-let timerId = null;
-
-startBtn.addEventListener("click", () => {
-  timerId = setInterval(() => {
-    console.log(`I love async JS!  ${Math.random()}`);
-  }, 1000);
-});
+const refs = {
+    modal: document.querySelector('#exampleModal'),
+    subscribeBtn: document.querySelector('button[data-subscribe]')
+}
 
 
-stopBtn.addEventListener("click", () => {
-  clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
-});
+// modal.show();
+
+refs.modal.addEventListener('hide.bs.modal', openModal);
+refs.subscribeBtn.addEventListener('click', handleSubscribeBtnClick);
+
+
+function openModal() {
+    if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+        return;
+}
+
+    setTimeout(() => {
+        console.log('откріваем надоедалку');
+        modal.show();
+        promptCounter += 1;
+    }, PROMPT_DELAY)
+};
+
+function handleSubscribeBtnClick() {
+    hasSubscribed = true;
+    modal.hide();
+};
+
+// const startBtn = document.querySelector(".js-start");
+// const stopBtn = document.querySelector(".js-stop");
+// let timerId = null;
+
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
 
 const date = new Date;
 
@@ -26,61 +62,61 @@ console.log(date);
 
 console.log(date.toString());
 
-const teamMeetingDate = new Date(2030, 2, 16, 14, 25, 0, 0);;
-console.log(teamMeetingDate);
-date.setDate(20);
-console.log(date);
-console.log("getDate(): ", date.getDate());
-console.log("getMonth(): ", date.getMonth());
-console.log("getFullYear(): ", date.getFullYear());
-console.log("getUTCMonth(): ", date.getUTCMonth());
+// const teamMeetingDate = new Date(2030, 2, 16, 14, 25, 0, 0);;
+// console.log(teamMeetingDate);
+// date.setDate(20);
+// console.log(date);
+// console.log("getDate(): ", date.getDate());
+// console.log("getMonth(): ", date.getMonth());
+// console.log("getFullYear(): ", date.getFullYear());
+// console.log("getUTCMonth(): ", date.getUTCMonth());
 
 
-console.log(date.toTimeString());
+// console.log(date.toTimeString());
 
-console.log('First');
-setTimeout( 
-    function () {
-        console.log('inner SetTimeout 1000')
-    }, 1000
-)
-console.log('Second');
+// console.log('First');
+// setTimeout( 
+//     function () {
+//         console.log('inner SetTimeout 1000')
+//     }, 1000
+// )
+// console.log('Second');
 
-setTimeout( 
-    function () {
-        console.log('inner SetTimeout 3000')
-    }, 3000
-)
+// setTimeout( 
+//     function () {
+//         console.log('inner SetTimeout 3000')
+//     }, 3000
+// )
 
-console.log('Third');
-let seconds = 1;
-const stopId = setInterval(() => {
-    console.log(seconds);
-    seconds+=1
-}, 1000);
+// console.log('Third');
+// let seconds = 1;
+// const stopId = setInterval(() => {
+//     console.log(seconds);
+//     seconds+=1
+// }, 1000);
 
-document.addEventListener('click', () => {
-    clearInterval(stopId);
-});
+// document.addEventListener('click', () => {
+//     clearInterval(stopId);
+// });
 
-console.dir(date);
+// console.dir(date);
 
-const weekDays = ['НД', "ПН", "ВТ", "СР", "чт", "ПТ", "СБ"];
-const getWeekDays = data => {
-    const weekDayIndex = data.getDay();
-    return weekDays[weekDayIndex];
-}
+// const weekDays = ['НД', "ПН", "ВТ", "СР", "чт", "ПТ", "СБ"];
+// const getWeekDays = data => {
+//     const weekDayIndex = data.getDay();
+//     return weekDays[weekDayIndex];
+// }
 
-const data = new Date();
-console.log(getWeekDays(data));
+// const data = new Date();
+// console.log(getWeekDays(data));
 
-const getLastDayOfMonth = (year, month) => {
-    const date = new Date(year, month + 1, 0);
+// const getLastDayOfMonth = (year, month) => {
+//     const date = new Date(year, month + 1, 0);
     
-    return date.getDate();
-};
+//     return date.getDate();
+// };
 
-console.dir(getLastDayOfMonth(2012, 1));
+// console.dir(getLastDayOfMonth(2012, 1));
 
 
 
